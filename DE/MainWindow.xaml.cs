595179868d6c7e8dbfe1b633b9f537f5d2f61323
NikitaDombrovky <nikita.nikita55@mail.ru>
2022-04-05 20:17:00 +0600

    first

---
+++ DE/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DE
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int _form = 1;
+        int s;
+        string _form2;
+        private Employee _employee = new Employee();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = _employee;
+            EmployeeDataGrid.ItemsSource = Расчет_премииEntities.GetContext().Employees.ToList();
+
+
+        }
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1(null);
+            window1.Show();
+            Hide();
+
+        }
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            if (EmployeeDataGrid.SelectedItem != null)
+            {
+                Window1 window1 = new Window1(EmployeeDataGrid.SelectedItem as Employee);
+                window1.Show();
+                Hide();
+            }
+            else
+            {
+                MessageBox.Show("!");
+            }
+        }
+        private void BtnDel_Click(object sender, RoutedEventArgs e)
+        {
+            if (EmployeeDataGrid.SelectedItem != null)
+            {
+                var employeeForRemoving = EmployeeDataGrid.SelectedItems.Cast<Employee>().ToList();
+                if (MessageBox.Show($"Вы точно хотите удалить следующее {employeeForRemoving.Count()} элементов?", "Внимание",
+                    MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        Расчет_премииEntities.GetContext().Employees.RemoveRange(employeeForRemoving);
+                        Расчет_премииEntities.GetContext().SaveChanges();
+                        MessageBox.Show("Данные удалены!");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выберите!");
+            }
+          
+        }
+    }
+}
---
+++ DE/Window1.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DE
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        //int s = 0;
+        int s = 0;
+        private Employee _employee = new Employee();
+        public Window1(Employee _selectedEmployess)
+        {
+            InitializeComponent();
+            s = 0;
+            if (_selectedEmployess != null)
+            {
+                s = 1;
+                _employee = _selectedEmployess;
+            }
+            DataContext = _employee;
+            ComboPost.ItemsSource = Расчет_премииEntities.GetContext().Positions.ToList();
+        }
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_employee.FIO))
+                errors.AppendLine("ФИО!");
+            if (string.IsNullOrWhiteSpace(Convert.ToString(_employee.Position)))
+                errors.AppendLine("Должность!");
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_employee.ID_Employee == 0)
+                Расчет_премииEntities.GetContext().Employees.Add(_employee);
+            try
+            {
+                Расчет_премииEntities.GetContext().SaveChanges();
+                if (s == 1)
+                {
+                    MessageBox.Show("Изменение успешно!");
+                }
+                else
+                {
+                    MessageBox.Show("Добавление успешно!");
+                }
+
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                Hide();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+
+            }
+        }
+    }
+}
